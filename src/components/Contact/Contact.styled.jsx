09de0da92c66f1acead38675d427c5e01a54ebdc
AllaGrey <alla.basanko2012@gmail.com>
2023-03-27 21:19:22 +0300

    add 2 task

---
+++ src/components/Contact/Contact.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Item = styled.li``;
+export const Button = styled.button`
+  margin-left: 10px;
+  width: 60px;
+  border: none;
+  padding: 3px;
+  border-radius: 10px;
+  background-color: red;
+  color: white;
+`;
